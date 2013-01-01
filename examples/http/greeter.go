009453382a1d881878c94bf92ce6417054d003c1
Anton Ageev <antage@gmail.com>
2013-01-01 23:59:18 +0400

    go fmt

--- examples/http/greeter.go
+++ examples/http/greeter.go
@@ -8,20 +8,20 @@ import (
 )
 
 func main() {
-    es := eventsource.New()
-    defer es.Close()
-    http.Handle("/", http.FileServer(http.Dir("./public")))
-    http.Handle("/events", es)
-    go func() {
-        for {
-            es.SendMessage("hello", "", "")
-            log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
-            time.Sleep(2 * time.Second)
-        }
-    }()
-    log.Print("Open URL http://localhost:8080/ in your browser.")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	es := eventsource.New()
+	defer es.Close()
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/events", es)
+	go func() {
+		for {
+			es.SendMessage("hello", "", "")
+			log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
+			time.Sleep(2 * time.Second)
+		}
+	}()
+	log.Print("Open URL http://localhost:8080/ in your browser.")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- http/consumer.go
+++ http/consumer.go
@@ -1,21 +1,21 @@
 package http
 
 import (
-    "net"
-    "net/http"
+	"net"
+	"net/http"
 )
 
 type consumer struct {
-    conn  net.Conn
-    close chan bool
+	conn  net.Conn
+	close chan bool
 }
 
 func newConsumer(resp http.ResponseWriter) (*consumer, error) {
-    conn, _, err := resp.(http.Hijacker).Hijack()
-    if err != nil {
-        return nil, err
-    }
-    conn.Write([]byte("HTTP/1.1 200 OK\nContent-Type: text/event-stream\nX-Accel-Buffering: no\n\n"))
+	conn, _, err := resp.(http.Hijacker).Hijack()
+	if err != nil {
+		return nil, err
+	}
+	conn.Write([]byte("HTTP/1.1 200 OK\nContent-Type: text/event-stream\nX-Accel-Buffering: no\n\n"))
 
-    return &consumer{conn, make(chan bool)}, nil
+	return &consumer{conn, make(chan bool)}, nil
 }
